cd897d816d6fa080981badd0f4ce0c8fa6984c19
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-02-15 08:54:22 +0600

    inputBoxCheck

--- script.js
+++ script.js
@@ -17,23 +17,24 @@ function onePlus(id) {
 }
 
 function oneMinus(id) {
-    const element = document.querySelector(`#${id}`);
+    const element  = document.querySelector(`#${id}`);
     const quantity = parseInt(element.value) - 1;
 
     if (quantity > -1) {
         element.value = quantity;   
     } else {
-        alert('Negative quantity not exist!');     
+        alert('Negative quantity not exist!');    
     }
 }
 
-first_class.addEventListener("input", () => {
-    const book_quantity = parseInt(first_class.value);
-     
-    if(empty(first_class.value)) {
-        first_class.value = 0;
-    } else if (book_quantity < 0) {
+function inputBoxCheck(id) {
+    const element = document.querySelector(`#${id}`);
+    const quantity = parseInt(element.value);
+    
+    if(empty(element.value)) {
+        element.value = 0;
+    } else if (quantity < 0) {
         alert('Negative quantity not exist!');
-        first_class.value = 0;
+        element.value = 0;
     } 
-});
+}
