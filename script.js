e2f4b7365cd954db40f53a87da0270c38af8307c
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-02-15 07:37:29 +0600

    selector

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(`hello world!`);
+const first_class = document.querySelector("#first_class");
+const plus        = document.querySelector("#plus");
+const minus       = document.querySelector("#minus");
+const economy     = document.querySelector("#economy");
+const plus2       = document.querySelector("#plus2");
+const minus2      = document.querySelector("#minus2");
+const subtotal    = document.querySelector("#subtotal");
+const total       = document.querySelector("#total");
+
