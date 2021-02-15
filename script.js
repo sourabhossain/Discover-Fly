7bb8253c920c753a99913fd4345216c2af69bc61
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-02-15 08:22:39 +0600

    First Class done

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ const minus2      = document.querySelector("#minus2");
 const subtotal    = document.querySelector("#subtotal");
 const total       = document.querySelector("#total");
 
+function empty(value) {
+    return (value == null || value === '');
+}
+
+plus.addEventListener("click", () => {
+    first_class.value = parseInt(first_class.value) + 1;
+});
+
+minus.addEventListener("click", () => {
+    const book_quantity = parseInt(first_class.value) - 1;
+
+    if (book_quantity > -1) {
+        first_class.value = book_quantity;   
+    } else {
+        alert('Negative quantity not exist!');     
+    }
+});
+
+first_class.addEventListener("input", () => {
+    const book_quantity = parseInt(first_class.value);
+     
+    if(empty(first_class.value)) {
+        first_class.value = 0;
+    } else if (book_quantity < 0) {
+        alert('Negative quantity not exist!');
+        first_class.value = 0;
+    } 
+});
